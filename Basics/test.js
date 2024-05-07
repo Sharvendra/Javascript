6d58655f4520f2f5167398dc5384e8f7cff950e0
Sharvendra <111687468+Sharvendra@users.noreply.github.com>
2024-05-07 21:46:04 +0000

    Conversion

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,17 @@
-console.log("hello")
+// console.log("hello")
+
+// let score = "33lm"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber)
+
+let nn=1
+
+let loggedIn = Boolean(nn)
+console.log(loggedIn)
+
+//1 => true, 0=>false
+// ""=> false, "nvyc"=> true
