5eadd3d7bea95b349314233965da815be4f530fd
Sharvendra <111687468+Sharvendra@users.noreply.github.com>
2024-05-07 22:22:35 +0000

    String

---
+++ Basics/String_1.js
@@ -0,0 +1,24 @@
+const name = "Sab"
+const reCount = 50
+
+// console.log(`Hello ${name} and ${reCount}`)
+
+const game = new String("abckugjhcgvd")
+
+// console.log(game.length)
+// console.log(game.charAt(2))
+// console.log(game.indexOf("b"))
+
+const newString= game.substring(0,4)
+// console.log(newString)
+
+const anotherString = game.slice(-8,4)
+// console.log(anotherString)
+
+// .replace
+// .includes
+// .split
+//
+
+
+
