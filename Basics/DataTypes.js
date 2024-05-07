0110bb0bb3089407c28b4f61f26e624b91c68f9e
Sharvendra <111687468+Sharvendra@users.noreply.github.com>
2024-05-07 22:08:54 +0000

    Data types

---
+++ Basics/DataTypes.js
@@ -0,0 +1,16 @@
+// Primitive
+// 7 types : String, Number, Boolean , null , undefined, Symbol, BigInt
+
+// Reference (Non primitive)
+
+// Array , Objects, Functions
+
+const a=["a", "b", "c"];
+let obj= {
+    name:"Sab",
+    age:12
+}
+
+const fun = function(){
+    
+}
--- Basics/test.js
+++ Basics/test.js
@@ -34,14 +34,14 @@ let str3 = str1+ str2
 
 // console.log("2" > 1);
 
-console.log(null > 0);
-console.log(null ==0);
-console.log(null >=0);
+// console.log(null > 0);
+// console.log(null ==0);
+// console.log(null >=0);
 
-console.log(undefined > 0);
+// console.log(undefined > 0);
 
-console.log(undefined ==0);
-console.log(undefined >=0);
+// console.log(undefined ==0);
+// console.log(undefined >=0);
 
-console.log("2" === 2)
+// console.log("2" === 2)
 
