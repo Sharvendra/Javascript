6086cf7e0d064730cf8dde68eac432f7b12e368d
Sharvendra <111687468+Sharvendra@users.noreply.github.com>
2024-05-07 22:00:08 +0000

    Comparisons

--- Basics/test.js
+++ Basics/test.js
@@ -8,10 +8,40 @@
 
 // console.log(valueInNumber)
 
-let nn=1
+// let nn=1
 
-let loggedIn = Boolean(nn)
-console.log(loggedIn)
+// let loggedIn = Boolean(nn)
+// console.log(loggedIn)
 
 //1 => true, 0=>false
 // ""=> false, "nvyc"=> true
+
+// ***********Operations *****************
+
+
+let str1 = "hello"
+let str2 = " sab"
+let str3 = str1+ str2
+// console.log(str3)
+
+// console.log("1"+ 2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(+"")
+
+// console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null ==0);
+console.log(null >=0);
+
+console.log(undefined > 0);
+
+console.log(undefined ==0);
+console.log(undefined >=0);
+
+console.log("2" === 2)
+
